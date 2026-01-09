47569e03694a18beadecfbf8f1f9ef87e091862c
ABDUL MANAF ALI <ohenemanaf@gmail.com>
2026-01-09 18:44:17 +0000

    Day 1 - Band Name Generator

---
+++ bandNameGenerator.py
@@ -0,0 +1,9 @@
+# Band name Generator
+
+
+# Accepting input from the User
+city = input("Which city where you born? \n")
+
+pet = input("What's the name of your Pet? \n")
+
+print("Your Band Name is " + city+ " " + pet)
