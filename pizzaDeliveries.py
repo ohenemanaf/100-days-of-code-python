bd555acb3f0ce4a3340a1de8cf43424719eb4b37
ABDUL MANAF ALI <ohenemanaf@gmail.com>
2026-03-11 08:11:50 +0000

    Day-04-pizzaDeliveries

--- pizzaDeliveries.py
+++ pizzaDeliveries.py
@@ -1,8 +1,10 @@
 print("Welcome to Python Pizza Deliveries!")
+
 size = input("What size pizza do you want? S, M, or L:  ")
 add_pepperoni = input("Do you want pepperoni? Y or N:  ")
 extra_cheese = input("Do you want extra cheese? Y or N:  ")
 bill = 0
+
 if size == "S":
     bill += 15
 elif size == "M":
@@ -18,4 +20,5 @@ elif add_pepperoni == "Y" and (size == "M" or size == "L"):
     bill += 3
 if extra_cheese == "Y":
     bill += 1
+    
 print(f"Your final bill is: ${bill}.")
