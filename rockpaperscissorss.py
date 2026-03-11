562e9be5c30a95fed367111549b0e46ad1e5f2c4
ABDUL MANAF ALI <ohenemanaf@gmail.com>
2026-03-11 06:07:18 +0000

    Refactor input handling and winner determination -> changed input number into int value instead of a string

--- rockpaperscissorss.py
+++ rockpaperscissorss.py
@@ -27,37 +27,41 @@ scissors = '''
 ---.__(___)
 
 '''
-choice = input("Welcome to Rock, Paper, Scissors!"
-      "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
-if choice == "0":
-    print(rock)
-elif choice == "1":
-    print(paper)
-elif choice == "2":
-    print(scissors)
-else:
+choice = int(input("Welcome to Rock, Paper, Scissors!\n"
+                   "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
+if choice >=3 or choice < 0:
     print("Invalid choice. Please choose 0, 1, or 2.")
-
-computer_choice = random.randint(0, 2)
-
-if computer_choice == 0:
-    print("Computer chose rock")
-    print(rock)
-elif computer_choice == 1:
-    print("Computer chose paper")
-    print(paper)
-elif computer_choice == 2:
-    print("Computer chose scissors")
-    print(scissors)
-
-# Determine the winner
-if choice == "0" and computer_choice == 2:
-    print("You win!")
-elif choice == "1" and computer_choice == 0:
-    print("You win!")
-elif choice == "2" and computer_choice == 1:
-    print("You win!")
-elif choice == computer_choice:
-    print("It's a draw!")
 else:
-    print("You lose!")
+    if choice == 0:
+        print(rock)
+    elif choice == 1:
+        print(paper)
+    elif choice == 2:
+        print(scissors)
+    else:
+        print("Invalid choice. Please choose 0, 1, or 2.")
+
+    computer_choice = random.randint(0, 2)
+
+    if computer_choice == 0:
+        print("Computer chose rock")
+        print(rock)
+    elif computer_choice == 1:
+        print("Computer chose paper")
+        print(paper)
+    elif computer_choice == 2:
+        print("Computer chose scissors")
+        print(scissors)
+
+    # Determine the winner
+    if choice == computer_choice:
+        print("It's a draw!")
+    elif choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif choice == 1 and computer_choice == 0:
+        print("You win!")
+    elif choice == 2 and computer_choice == 1:
+        print("You win!")
+
+    else:
+        print("You lose!")
